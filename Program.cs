5c0e791aedee14f99ea9c705d9b0da21cbc0273c
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-11 23:05:24 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double [] nums = {1.25, 1241.53, 5.44, 90.21, 6.51};
+
+           Console.WriteLine(nums[2]);
+
+           Console.ReadLine();
+        } 
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Arrays Kata
+
+Another console kata. 
+
+The purpose of this is to get more practice setting up C# projects and go over the basics again.
